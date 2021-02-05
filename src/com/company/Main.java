4e0fd8c0a64f8ac1779e2dc4498a1ff8883d9d3f
Dylan <dzafrar01@elpuig.xeill.net>
2021-02-05 21:22:04 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package Lambda;
+import lambdas.Persona;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public static void print (Persona persona){
+            System.out.println(personag);
+        }
+
+        public static void Main (String[]args){
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("introdueix la data: 00/00/0000");
+
+            lambdas.Persona persona1 = new lambdas.Persona("Dylan", Persona.Genere.HOME, LocalDate.parse("25/12/2002", format));
+
+            lambdas.Persona persona2 = new lambdas.Persona("Marc", lambdas.Persona.Genere.HOME, LocalDate.parse("12/10/1980", format));
+
+            lambdas.Persona persona3 = new lambdas.Persona("Oriol", Persona.Genere.HOME, LocalDate.parse("10/01/1984", format));
+
+            lambdas.Persona persona4 = new lambdas.Persona("Diana", Persona.Genere.DONA, LocalDate.parse("24/04/1974", format));
+
+            lambdas.Persona persona5 = new lambdas.Persona("CARLA", lambdas.Persona.Genere.DONA, LocalDate.parse("24/04/1992", format));
+
+            List<lambdas.Persona> llistaPersones = new ArrayList<>();
+
+            llistaPersones.add(persona1);
+
+            llistaPersones.add(persona2);
+
+            llistaPersones.add(persona3);
+
+            llistaPersones.add(persona4);
+
+            llistaPersones.add(persona5);
+        }
+    }
+}
---
+++ src/com/company/Persona.java
@@ -0,0 +1,76 @@
+package Lambda;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Persona implements Comparable<Persona> {
+
+    private String nompersona;
+    private LocalDate naixementpersona;
+    private Generepersona generepersona;
+
+    public enum Generepersona {DONA,HOME;}
+
+    public Persona(String nompersona, Generepersona generepersona, LocalDate naixementpersona) {
+        this.nompersona = nompersona;
+        this.generepersona = generepersona;
+        this.naixementpersona = naixementpersona;
+    }
+
+    public String getNompersona() {
+        return nompersona;
+    }
+
+
+    public void setNompersona(String nompersona) {
+        this.nompersona = nompersona;
+    }
+
+
+    public LocalDate getNaixementpersona() {
+        return naixementpersona;
+    }
+
+
+    public void setNaixementpersona(LocalDate naixementpersona) {
+        this.naixementpersona = naixementpersona;
+    }
+
+
+    public Generepersona getGenerepersona() {
+        return generepersona;
+    }
+
+
+    public void setGenerepersona(Generepersona generepersona) {
+        this.generepersona = generepersona;
+    }
+
+
+    @Override
+    public int compareTo(Persona o) {
+        if(getAge() > o.getAge()) return -1;
+        else if(getAge() < o.getAge()) return 1;
+        else return 0;
+    }
+
+    public int getAge() {
+        LocalDate today = LocalDate.now();
+        if ((naixementpersona != null) && (today != null)) {
+            return Period.between(naixementpersona, today).getYears();
+        } else {
+            return 0;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return nompersona + '\t' + generepersona.toString() + '\t' + getAge();
+    }
+
+
+
+
+
+}
